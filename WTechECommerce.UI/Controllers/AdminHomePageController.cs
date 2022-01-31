415d01b24096b53a6c3a53e2eaf00129536f3b1f
Eray Nurtekin <eray.nurtekin@hotmail.com>
2022-01-31 16:10:50 +0300

    Admin Template Yapıldı

---
+++ WTechECommerce.UI/Controllers/AdminHomePageController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WTechECommerce.UI.Controllers
+{
+    public class AdminHomePageController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
