411dc9910ad89e4742061fe7405c15143ee4f405
Eray Nurtekin <eray.nurtekin@hotmail.com>
2022-02-03 09:14:16 +0300

    Admin Page Add Template giydirildi

---
+++ WTechECommerce.UI/Controllers/AdminHomePageController.cs
@@ -0,0 +1,33 @@
+<<<<<<< HEAD:WTechECommerce.UI/Areas/Admin/Controllers/AdminHomePageController.cs
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WTechECommerce.UI.Models.Helper;
+
+namespace WTechECommerce.UI.Controllers
+{
+    public class AdminHomePageController : Controller
+    {
+        public IActionResult Index()
+        {
+    
+            return View();
+        }
+    }
+}
+=======
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WTechECommerce.UI.Models.Helper;
+
+namespace WTechECommerce.UI.Controllers
+{
+    public class AdminHomePageController : Controller
+    {
+        public IActionResult Index()
+        {
+    
+            return View();
+        }
+    }
+}
+>>>>>>> 7255a7660995386d6668b2614e2d25f6b41598a3:WTechECommerce.UI/Controllers/AdminHomePageController.cs
